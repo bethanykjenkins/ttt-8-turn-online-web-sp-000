0caf499f0adc2018d97b5b114624ae7fc0f3a752
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-08-29 23:06:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,10 +34,13 @@ def turn(board)
  puts "Please enter 1-9:"
  input = gets.strip
  index = input_to_index(input)
-  until valid_move?(board, index) == true
-   puts "Please enter 1-9:"
-  input = gets.strip
- index = input_to_index(input)
- end
+    until valid_move?(board, index) == true
+      puts "Please enter 1-9:"
+      input = gets.strip
+      index = input_to_index(input)
+    end
+    if valid_move?(board, index) == true
+      puts display_board(board)
+    end
 end
 
